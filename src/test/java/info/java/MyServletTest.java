e6c65c24f0f9978c73c1ae0e7890999578382a6a
SaiKrishna2681 <45065762+SaiKrishna2681@users.noreply.github.com>
2020-03-05 12:58:01 +0530

    Update MyServletTest.java

--- src/test/java/info/java/MyServletTest.java
+++ src/test/java/info/java/MyServletTest.java
@@ -6,7 +6,7 @@ import static org.mockito.Mockito.when;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-
+import java.sql.*;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
